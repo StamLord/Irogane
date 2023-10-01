ba258c9bb86077d06eb596dcd96b66065b767133
StamLord <42681607+StamLord@users.noreply.github.com>
2023-10-02 00:32:12 +0300

    CRPG Dialogue Update
    * Text is now added inside a scroll container instead of being replaced in the same node
    * Each line is now preceded by the character's name and ends with a new line

--- Irogane/addons/dialogue_manager/example_balloon/crpg_balloon.gd
+++ Irogane/addons/dialogue_manager/example_balloon/crpg_balloon.gd
@@ -3,8 +3,12 @@ extends CanvasLayer
 
 @onready var balloon: Panel = %Balloon
 @onready var character_label: RichTextLabel = %CharacterLabel
-@onready var dialogue_label: DialogueLabel = %DialogueLabel
 @onready var responses_menu: CRPGDialogueResponsesMenu = %ResponsesMenu
+@onready var dialogue_container = %dialogue_container
+@onready var dialogue_label_asset = preload("res://addons/dialogue_manager/dialogue_label.tscn")
+@onready var scroll_container = %scroll_container
+@onready var scrollbar = scroll_container.get_v_scroll_bar()
+var current_dialogue_label
 
 ## The dialogue resource
 var resource: DialogueResource
@@ -29,13 +33,19 @@ var dialogue_line: DialogueLine:
 			return
 
 		dialogue_line = next_dialogue_line
-
-		character_label.visible = not dialogue_line.character.is_empty()
-		character_label.text = tr(dialogue_line.character, "dialogue")
-
-		dialogue_label.hide()
-		dialogue_label.dialogue_line = dialogue_line
-
+		
+		# Create new dialogue label
+		current_dialogue_label = create_dialogue_label()
+		
+		# Add character name
+		dialogue_line.text = dialogue_line.character.capitalize() + ": " + dialogue_line.text
+		
+		# Add new line after each node
+		dialogue_line.text += "\n "
+		
+		# Set text
+		current_dialogue_label.dialogue_line = dialogue_line
+		
 		responses_menu.hide()
 		responses_menu.set_responses(dialogue_line.responses)
 
@@ -43,10 +53,10 @@ var dialogue_line: DialogueLine:
 		balloon.show()
 		will_hide_balloon = false
 
-		dialogue_label.show()
+		current_dialogue_label.show()
 		if not dialogue_line.text.is_empty():
-			dialogue_label.type_out()
-			await dialogue_label.finished_typing
+			current_dialogue_label.type_out()
+			await current_dialogue_label.finished_typing
 
 		# Wait for input
 		if dialogue_line.responses.size() > 0:
@@ -67,6 +77,9 @@ var dialogue_line: DialogueLine:
 func _ready() -> void:
 	balloon.hide()
 	Engine.get_singleton("DialogueManager").mutated.connect(_on_mutated)
+	
+	# Scroll to botton whenever we add new items to scroll container
+	scrollbar.connect("changed", scroll_to_bottom)
 
 
 func _unhandled_input(_event: InputEvent) -> void:
@@ -80,12 +93,28 @@ func start(dialogue_resource: DialogueResource, title: String, extra_game_states
 	is_waiting_for_input = false
 	resource = dialogue_resource
 	self.dialogue_line = await resource.get_next_dialogue_line(title, temporary_game_states)
+	
+	# Set character label to first character
+	character_label.text = dialogue_resource.character_names[0].capitalize()
 
 
 ## Go to the next line
 func next(next_id: String) -> void:
 	self.dialogue_line = await resource.get_next_dialogue_line(next_id, temporary_game_states)
 
+
+# Create new entry
+func create_dialogue_label():
+	var new_label = dialogue_label_asset.instantiate()
+	dialogue_container.add_child(new_label)
+	
+	# Move before last (Responses)
+	dialogue_container.move_child(new_label, dialogue_container.get_child_count() - 2)
+	
+	return new_label
+
+func scroll_to_bottom():
+	scroll_container.scroll_vertical = scrollbar.max_value
 
 ### Signals
 
@@ -102,9 +131,9 @@ func _on_mutated(_mutation: Dictionary) -> void:
 
 func _on_balloon_gui_input(event: InputEvent) -> void:
 	# If the user clicks on the balloon while it's typing then skip typing
-	if dialogue_label.is_typing and event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed():
+	if current_dialogue_label.is_typing and event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed():
 		get_viewport().set_input_as_handled()
-		dialogue_label.skip_typing()
+		current_dialogue_label.skip_typing()
 		return
 
 	if not is_waiting_for_input: return
