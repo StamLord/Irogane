81c741bb5eedeb02aa67a500f91dbbc61b898f92
Daniel <idanielzor@gmail.com>
2023-12-17 10:49:49 +0200

    decouple settings window and add settings to main menu

--- Irogane/scripts/ui/main_menu.gd
+++ Irogane/scripts/ui/main_menu.gd
@@ -2,6 +2,7 @@ extends UIWindow
 
 @onready var scene_manager = get_node("/root/SceneManager")
 @onready var load_window = %load_window
+@onready var settings_window = %settings_window
 
 func _ready():
 	visible = true
@@ -20,3 +21,6 @@ func _on_load_button_pressed():
 func _on_quit_button_pressed():
 	get_tree().quit()
 	
+
+func _on_settings_button_pressed():
+	settings_window.open()
