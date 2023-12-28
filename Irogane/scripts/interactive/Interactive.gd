65ea2fd5c469ac5171f020ce289e7265f386ecf0
StamLord <42681607+StamLord@users.noreply.github.com>
2023-12-28 10:36:47 +0200

    Interactive - Added highlight support for ShaderMaterial

--- Irogane/scripts/interactive/Interactive.gd
+++ Irogane/scripts/interactive/Interactive.gd
@@ -15,17 +15,23 @@ var base_emission_operation = null
 
 func _ready():
 	if mesh != null:
-		# Set a clone of the active material as override to make local changes
-		active_material = mesh.get_surface_override_material(0).duplicate()
+		active_material = mesh.get_surface_override_material(0)
 		
+		if active_material == null:
+			return
+		
+		# Set a clone of the active material as override to make local changes
+		active_material = active_material.duplicate()
 		mesh.set_surface_override_material(0, active_material)
 		
 		# Get defaults to turn back off
-		base_emission_enabled = active_material.emission_enabled
-		base_emission_energy_multiplier = active_material.emission_energy_multiplier
-		base_emission_texture = active_material.emission_texture
-		base_emission_operation = active_material.emission_operator
-		
+		if active_material is StandardMaterial3D:
+			base_emission_enabled = active_material.emission_enabled
+			base_emission_energy_multiplier = active_material.emission_energy_multiplier
+			base_emission_texture = active_material.emission_texture
+			base_emission_operation = active_material.emission_operator
+		elif active_material is ShaderMaterial:
+			base_emission_energy_multiplier = active_material.get_shader_parameter("emission_multiplier")
 	
 
 func get_text():
@@ -37,18 +43,29 @@ func use(interactor):
 	
 
 func highlight_on():
-	if active_material != null:
+	if active_material == null:
+		return
+	
+	if active_material is StandardMaterial3D:
 		active_material.emission_enabled = true
 		var new_emission_multiplier = HIGHLIGHT_EMMISION if base_emission_energy_multiplier == 0 else base_emission_energy_multiplier * 3
 		active_material.emission_energy_multiplier = new_emission_multiplier
 		active_material.emission_operator = StandardMaterial3D.EMISSION_OP_MULTIPLY
 		active_material.emission_texture = active_material.albedo_texture
+	elif active_material is ShaderMaterial:
+		var new_emission_multiplier = HIGHLIGHT_EMMISION if base_emission_energy_multiplier == 0 else base_emission_energy_multiplier * 3
+		active_material.set_shader_parameter("emission_multiplier", new_emission_multiplier)
 	
 
 func highlight_off():
-	if active_material != null:
+	if active_material == null:
+		return
+	
+	if active_material is StandardMaterial3D:
 		active_material.emission_enabled = base_emission_enabled
 		active_material.emission_energy_multiplier = base_emission_energy_multiplier
 		active_material.emission_operator = base_emission_operation
 		active_material.emission_texture = base_emission_texture
+	elif active_material is ShaderMaterial:
+		active_material.set_shader_parameter("emission_multiplier", base_emission_energy_multiplier)
 	
