36914948e36293ff9ca81a70ae44cc5cbf51632c
StamLord <42681607+StamLord@users.noreply.github.com>
2024-01-22 20:12:29 +0200

    Update skill_tree.gd
    * Refactored & added save_data/load_data

--- Irogane/scripts/skills/skill_tree.gd
+++ Irogane/scripts/skills/skill_tree.gd
@@ -3,7 +3,6 @@ extends Control
 class_name SkillTree
 
 @onready var skill_tree_manager = $".."
-var skills = []
 var skill_dict = {}
 
 func _ready():
@@ -11,17 +10,14 @@ func _ready():
 	
 
 func init_skill_dict():
-	skills = get_children()
-	
-	for skill in skills:
-		if skill is Skill:
-			skill_dict[skill.name] = skill
+	for child in get_children():
+		if child is Skill:
+			skill_dict[child.name] = child
 	
 
 func update_state(skill_name : String):
-	for skill in skills:
-		if skill is Skill:
-			skill.update_state(skill_name)
+	for skill in skill_dict:
+		skill_dict[skill].update_state(skill_name)
 	
 
 func get_skill(skill_name : String):
@@ -40,3 +36,27 @@ func decrease_skill_points(amount):
 
 func increase_skill_points(amount):
 	skill_tree_manager.increase_skill_points(amount)
+	
+
+# Return an array of skill names (strings)
+func get_learned_skills():
+	var learned = []
+	for skill in skill_dict:
+		if skill_dict[skill].is_learned:
+			learned.append(skill)
+	
+	return learned
+	
+
+func save_data():
+	return {"learned": get_learned_skills()}
+	
+
+func load_data(data):
+	if data.has("learned") == false:
+		return
+	
+	for skill_name in data["learned"]:
+		if skill_dict.has(skill_name):
+			skill_dict[skill_name].learn()
+	
