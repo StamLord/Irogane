9d2a0a919c7831e196c78fc52f0a0076a8702724
StamLord <42681607+StamLord@users.noreply.github.com>
2024-01-22 16:04:13 +0200

    skill_croll.gd - Added lerp on ohver

--- Irogane/scripts/ui/skill_scroll.gd
+++ Irogane/scripts/ui/skill_scroll.gd
@@ -3,6 +3,9 @@ extends TextureButton
 var initial_pos
 var selected = false
 @export var skill_name = ""
+@export var hover_offset = -75.0
+@export var select_offset = -100.0
+@export var duration = 0.02
 
 signal skill_selected(skill_name)
 
@@ -16,16 +19,14 @@ func _mouse_entered():
 	if selected:
 		return
 	
-	var curr_pos = initial_pos
-	curr_pos.x -= 10
-	set_position(curr_pos)
+	animate_offset(initial_pos.x + hover_offset, duration)
 	
 
 func _mouse_exited():
 	if selected:
 		return
 	
-	set_position(initial_pos)
+	animate_offset(initial_pos.x, duration)
 	
 
 func select():
@@ -33,9 +34,9 @@ func select():
 		return
 	
 	selected = true
-	var curr_pos = initial_pos
-	curr_pos.x -= 20
-	set_position(curr_pos)
+	var new_pos = initial_pos
+	new_pos.x += select_offset
+	set_position(new_pos)
 	
 
 func deselect():
@@ -46,4 +47,16 @@ func deselect():
 func _pressed():
 	select()
 	skill_selected.emit(skill_name)
+	
+
+func animate_offset(target_x, duration):
+	var start_time = Time.get_ticks_msec()
+	var start_x = position.x
+	duration *= 1000
+	while Time.get_ticks_msec() <= start_time + duration:
+		var t = (Time.get_ticks_msec() - start_time) / duration
+		position.x = lerp(start_x, target_x, t)
+		await get_tree().process_frame
+		
+	position.x = target_x
 	
