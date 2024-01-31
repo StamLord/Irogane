c6f4f597aaced97ae095f11ae0866dd703c38573
StamLord <42681607+StamLord@users.noreply.github.com>
2024-01-31 12:07:05 +0200

    Camera zoom fixed
    * Added face_camera for zooming
    * Moved camera default position
    * Default model rotation fixed to face camera
    * Fixed zoom & panning relation - More zoom equals less panning
    * Added lerp to smoothen final camera position

--- Irogane/scripts/ui/char_creation/camera_control_panel.gd
+++ Irogane/scripts/ui/char_creation/camera_control_panel.gd
@@ -3,6 +3,7 @@ extends Panel
 @onready var camera = %Camera3D
 @onready var reset_camera_button = %ResetCamera
 @onready var node_to_rotate = %human_model #%rotate_around
+@onready var face_camera = %face_camera
 
 # Camera and character movement logic
 var default_camera_position = Vector3(0, 1.8, 2)
@@ -18,6 +19,7 @@ const MAX_HORIZONTAL_MOVE = 0.5
 
 var camera_zoom_position = Vector3.ZERO
 var camera_pan_position = Vector3.ZERO
+var camera_pan_position_zoom_corrected = Vector3.ZERO
 
 var camera_pan_horizontal_percentage = 0.5
 var camera_pan_vertical_percentage = 0.5
@@ -42,66 +44,68 @@ func _process(delta):
 		if in_control_panel:
 			rotating = true
 			prev_mouse_position = get_viewport().get_mouse_position()
-
+	
 	if Input.is_action_just_released("Rotate"):
 		rotating = false
-
+	
 	if Input.is_action_just_pressed("Move"):
 		# Only control characer and camera in central panel
 		if in_control_panel:
 			moving = true
 			prev_mouse_position = get_viewport().get_mouse_position()
-
+	
 	if Input.is_action_just_released("Move"):
 		moving = false
-
+	
 	if rotating:
 		reset_camera_button.show()
 		next_mouse_position = get_viewport().get_mouse_position()
 		node_to_rotate.rotate_y((next_mouse_position.x - prev_mouse_position.x) * .4 * delta)
 		prev_mouse_position = next_mouse_position
-
+	
 	if moving:
 		reset_camera_button.show()
 		next_mouse_position = get_viewport().get_mouse_position()
-
+	
 		var mouse_delta = next_mouse_position - prev_mouse_position
-
+	
 		# Horizontal panning
 		camera_pan_horizontal_percentage -= mouse_delta.x * .2 * delta
 		camera_pan_horizontal_percentage = clampf(camera_pan_horizontal_percentage, 0.0, 1.0)
-
+	
 		# Vertical panning
 		camera_pan_vertical_percentage += mouse_delta.y * .3 * delta
 		camera_pan_vertical_percentage = clampf(camera_pan_vertical_percentage, 0.0, 1.0)
-
+		
 		camera_pan_position.x = lerp(-MAX_HORIZONTAL_MOVE, MAX_HORIZONTAL_MOVE, camera_pan_horizontal_percentage)
 		camera_pan_position.y = lerp(MIN_VERTICAL_MOVE, MAX_VERTICAL_MOVE, camera_pan_vertical_percentage)
-
+		
 		prev_mouse_position = next_mouse_position
-
+	
 	# Zoom in
 	if Input.is_action_just_pressed("ScrollUp"):
 		if in_control_panel:
 			reset_camera_button.show()
 			camera_zoom_percentage -= SCROLL_SPEED
 			camera_zoom_percentage = max(camera_zoom_percentage, 0.0)
-
-
+	
 	# Zoom out
 	if Input.is_action_just_pressed("ScrollDown"):
 		if in_control_panel:
 			reset_camera_button.show()
 			camera_zoom_percentage += SCROLL_SPEED
 			camera_zoom_percentage = min(camera_zoom_percentage, 1.0)
-
-
+	
+	# The more zoomed we are, the less panning we want
+	camera_pan_position_zoom_corrected = lerp(camera_pan_position * 0.1, camera_pan_position, camera_zoom_percentage)
+	
 	# Get zoom position that is added to default position
-	camera_zoom_position = lerp(Vector3(0,0,MIN_SCROLL), Vector3(0,0,MAX_SCROLL), camera_zoom_percentage)
-
+	camera_zoom_position = lerp(face_camera.position, default_camera_position, camera_zoom_percentage)
+	
 	# Get final camera position
 	if not animating_camera:
-		camera.position = default_camera_position + (camera.basis * (camera_zoom_position + camera_pan_position))
+		var new_position = camera_zoom_position + (camera.basis * camera_pan_position_zoom_corrected)
+		camera.position = lerp(camera.position, new_position, 30 * delta)
 	
 
 func _on_mouse_entered():
