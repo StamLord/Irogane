77b11e706089e4e275a6ab41aae58de54d74bb02
Daniel <idanielzor@gmail.com>
2024-02-03 21:39:54 +0200

    throwable wip

--- Irogane/scripts/item/item_db.gd
+++ Irogane/scripts/item/item_db.gd
@@ -2,38 +2,44 @@ extends Node
 class_name ItemDB
 
 const ICON_PATH = "res://assets/textures/icons/"
-const PICKUP_PATH = "res://assets/prefabs/pickups/"
+const PICKUP_PATH = "res://prefabs/pickups/"
 
 const ITEMS = {
 	"katana" : {
 		"icon" : ICON_PATH + "katana.png",
 		"slot" : "WEAPON",
 		"type" : "SWORD",
-		"pickup" : PICKUP_PATH + "pickup.tscn"
+		"pickup" : preload(PICKUP_PATH + "pickup.tscn")
 	},
 	"onigiri" : {
 		"icon" : ICON_PATH + "onigiri.png",
 		"slot" : "CONSUMABLE",
 		"type" : "CONSUMABLE",
-		"pickup" : PICKUP_PATH + "pickup.tscn"
+		"pickup" : preload(PICKUP_PATH + "pickup.tscn")
+	},
+	"shuriken" : {
+		"icon" : ICON_PATH + "shuriken.png",
+		"slot" : "WEAPON",
+		"type" : "THROWABLE",
+		"pickup" : preload(PICKUP_PATH + "weapons/shuriken.tscn")
 	},
 	"robes" : {
 		"icon" : ICON_PATH + "robes_template.png",
 		"slot" : "TORSO",
 		"type" : "EQUIPMENT",
-		"pickup" : PICKUP_PATH + "pickup.tscn"
+		"pickup" : preload(PICKUP_PATH + "pickup.tscn")
 	},
 	"godot cube" : {
 		"icon" : ICON_PATH + "godot_black.png",
 		"slot" : "NONE",
 		"type" : "NONE",
-		"pickup" : PICKUP_PATH + "pickup.tscn"
+		"pickup" : preload(PICKUP_PATH + "pickup.tscn")
 	},
 	"error" : {
 		"icon" : ICON_PATH + "godot_black.png",
 		"slot" : "NONE",
 		"type" : "NONE",
-		"pickup" : PICKUP_PATH + "pickup.tscn"
+		"pickup" : preload(PICKUP_PATH + "pickup.tscn")
 	},
 }
 
---
+++ Irogane/scripts/weapons/throwable.gd
@@ -0,0 +1,26 @@
+extends Node3D
+
+const item_base = preload("res://scripts/ui/item_base.tscn")
+const pickup_base = preload("res://prefabs/pickups/pickup.tscn")
+@onready var drop_origin = PlayerEntity.player_node
+var drop_offset = Vector3(0, 1.8, -1.5)
+@export var throw_force = 15.0
+
+func _process(delta):
+	if not visible:
+		return
+		
+	if Input.is_action_just_pressed("attack_primary"):
+		var item_id = "shuriken"
+		var pickup =  ItemDB.get_item(item_id)["pickup"].instantiate()
+		get_tree().get_root().add_child(pickup)
+		
+		pickup.global_position = drop_origin.global_position + drop_origin.get_global_transform().basis * drop_offset
+		
+		# Set item
+		for child in pickup.get_children():
+			if child is Pickup:
+				child.item_id = item_id
+		
+		pickup.linear_velocity = global_transform.basis * Vector3.FORWARD * throw_force
+	
