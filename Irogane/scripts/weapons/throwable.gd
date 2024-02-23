3ab6b9f8685cc300910e21334d13ac4d80773718
StamLord <42681607+StamLord@users.noreply.github.com>
2024-02-23 14:24:08 +0200

    ring_menu refactored
    * shurtiken ring menu added
    * weapon_manager.gd - removed ring menu handling

--- Irogane/scripts/weapons/throwable.gd
+++ Irogane/scripts/weapons/throwable.gd
@@ -1,9 +1,19 @@
 extends Node3D
 
+# Prefab
 @onready var projectile_scene = load("res://prefabs/weapons/projectiles/shuriken.tscn")
+
+# Skill Menu
+@onready var ring_menu = $shuriken_ring_menu
 
 const ITEM_ID = "shuriken"
 const INITIAL_POS_OFFSET = Vector3(0, 0, -0.5)
+
+var current_skill = ""
+
+func _ready():
+	ring_menu.item_selected.connect(skill_selected)
+	
 
 func fire_shuriken(position_offset, rotation_offset):
 	var projectile =  projectile_scene.instantiate()
@@ -19,17 +29,27 @@ func _process(delta):
 	if not visible:
 		return
 	
+	if Input.is_action_just_pressed("ring_menu") and not ring_menu.visible:
+		#var ring_items: Array = PlayerEntity.get_skills_in_tree("throw")
+		var ring_items : Array[String] = ["triple_throw", "octo_throw"]
+		if ring_items:
+			ring_menu.initialize_items(ring_items)
+			ring_menu.open()
+	elif not Input.is_action_pressed("ring_menu") and ring_menu.visible:
+		ring_menu.close()
+	
 	if Input.is_action_just_pressed("attack_primary"):
 		var position_offset = INITIAL_POS_OFFSET
 		var rotation_offset = Vector3.ZERO
 		fire_shuriken(position_offset, rotation_offset)
 	elif Input.is_action_just_pressed("attack_secondary"):
-		if get_parent().active_skill:
-			print("activating skill ", get_parent().active_skill)
-			if get_parent().active_skill == "triple_throw":
+		if current_skill:
+			print("activating skill ", current_skill)
+			if current_skill == "triple_throw":
 				triple_throw()
-			elif get_parent().active_skill == "octo_throw":
+			elif current_skill == "octo_throw":
 				octo_throw()
+	
 
 func throw_many_shuriken(pos_offset_array: Array, rotation_offset_array: Array):
 	if pos_offset_array.size() != rotation_offset_array.size():
@@ -92,4 +112,9 @@ func octo_throw():
 		rotation_offsets.append(rotation_offset)
 	
 	throw_many_static_shuriken(position_offsets, rotation_offsets)
+	
+
+func skill_selected(skill_name):
+	current_skill = skill_name
+	print(skill_name)
 	
