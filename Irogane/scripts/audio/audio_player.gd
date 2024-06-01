7b1d6967ac8a16f8c4764fd53f02c10bdcd0efd6
Daniel <idanielzor@gmail.com>
2024-06-02 02:07:34 +0300

    refactor audio player script, and some small fixes

--- Irogane/scripts/audio/audio_player.gd
+++ Irogane/scripts/audio/audio_player.gd
@@ -1,53 +1,79 @@
 extends Node3D
 class_name AudioPlayer
 
-@onready var audio_players = get_children()
-var currently_playing = []
+@export var bus: StringName = &"Master"
+@export var default_volume: float = 0.0
+var audio_players_by_id = {}
+var can_play = true
+@onready var playush = %playush
 
-func _ready():
-	for p in audio_players:
-		p.finished.connect(reset_position.bind(p))
+func set_can_play(value: bool):
+	can_play = value
 	
 
-func play_exclusive(sound, sound_position = null, pitch: float = 1):
-	if currently_playing.size() > 0:
+func get_can_play():
+	return can_play
+	
+
+func play_only_if_no_one_playing(sound: AudioStream, pitch: float = 1.0, volume: float = 0.0):
+	if audio_players_by_id.size() > 0:
 		return
 	
-	play(sound, sound_position, pitch)
+	play(sound, pitch, volume)
 	
 
-func play(sound, sound_position = null, pitch: float = 1.0, volume = 0.0 ):
-	if audio_players == null:
+func play_exclusive(sound: AudioStream, pitch: float = 1.0, volume: float = 0.0):
+	# TODO: FIX this
+	for id in audio_players_by_id:
+		var player = audio_players_by_id[id]
+		player.stop()
+		delete_player(player)
+	play(sound, pitch, volume)
+	
+
+func play(sound: AudioStream, pitch = null, volume: float = 0.0):
+	if not can_play:
 		return
 	
-	for p in audio_players:
-		if not p.playing:
-			if sound_position and p is Node3D:
-				p.global_position = sound_position
-			
-			p.stream = sound
-			
-			if pitch:
-				p.pitch_scale = pitch
-			
-			if volume:
-				p.volume_db = volume
-			
-			p.play()
-			
-			# Must avoid duplicate entries since we remove only the first occurance later
-			if not currently_playing.has(p):
-				currently_playing.append(p)
-			
-			return
-	
-	print(get_parent().name + "." + name + ": Not enought stream players!")
+	var player: AudioStreamPlayer = AudioStreamPlayer.new()
+	activate_player(player, sound, pitch, volume)
 	
 
-func reset_position(audio_player):
-	# Reset to local 0,0,0
-	if audio_player is Node3D:
-		audio_player.position = Vector3.ZERO
+func activate_player(player, sound: AudioStream, pitch = 1.0, volume: float = 0.0):
+	audio_players_by_id[player.get_instance_id()] = player
+	add_child(player)
 	
-	currently_playing.erase(audio_player)
+	
+	if pitch:
+		player.set_pitch_scale(pitch)
+	
+	player.set_volume_db(default_volume)
+	
+	if volume:
+		player.set_volume_db(volume)
+	
+	player.set_bus(bus)
+	player.set_stream(sound)
+	
+	player.play()
+	player.finished.connect(delete_player.bind(player))
+	
+
+func play_positional_sound(sound: AudioStream, sound_position = null, pitch = null, volume: float = 0.0):
+	if not can_play:
+		return
+	
+	var player: AudioStreamPlayer3D  = AudioStreamPlayer3D.new()
+	
+	if sound_position:
+		player.set_global_position(sound_position)
+	else:
+		player.set_global_position(global_position)
+	
+	activate_player(player, sound, pitch, volume)
+	
+
+func delete_player(audio_player: AudioStreamPlayer3D):
+	audio_players_by_id.erase(audio_player.get_instance_id())
+	audio_player.queue_free()
 	
