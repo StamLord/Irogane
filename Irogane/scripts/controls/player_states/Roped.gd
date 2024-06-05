d92f36f555adb09dfe1b879e3abe4dac2d41d87d
StamLord <42681607+StamLord@users.noreply.github.com>
2024-06-05 12:26:59 +0300

    Roped state swing

--- Irogane/scripts/controls/player_states/Roped.gd
+++ Irogane/scripts/controls/player_states/Roped.gd
@@ -15,11 +15,17 @@ class_name Roped
 @export var roped_move_speed = 8
 @export var jump_force = 2.0
 @export var push_force = 4
+@export var swing_force = 4.0
+@export var swing_rate = 1
 
 var direction = Vector3.ZERO
 var speed = 0
 
 var wall_normal = Vector3.ZERO
+var last_swing_left = 0
+var last_swing_right = 0
+var last_swing_backward = 0
+var last_swing_forward = 0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -43,12 +49,24 @@ func PhysicsUpdate(body, delta):
 	velocity.y -= gravity * delta
 	
 	# If we are moving near wall, we want snappy acceleration
-	if wall_check.is_colliding() and input_dir:
-		velocity.x = lerp(velocity.x, direction.x * speed, delta * roped_move_acceleration)
-		velocity.z = lerp(velocity.z, direction.z * speed, delta * roped_move_acceleration)
+	if wall_check.is_colliding():
+		if input_dir:
+			velocity.x = lerp(velocity.x, direction.x * speed, delta * roped_move_acceleration)
+			velocity.z = lerp(velocity.z, direction.z * speed, delta * roped_move_acceleration)
 	else:
-		velocity.x = lerp(velocity.x, direction.x * speed, delta * roped_acceleration)
-		velocity.z = lerp(velocity.z, direction.z * speed, delta * roped_acceleration)
+		# One time boost for swinging in air
+		if Input.is_action_just_pressed("left") and time_passed(last_swing_left, swing_rate):
+			velocity += body.basis * Vector3(-swing_force, 0.0, 0.0)
+			last_swing_left = Time.get_ticks_msec()
+		elif Input.is_action_just_pressed("right") and time_passed(last_swing_right, swing_rate):
+			velocity += body.basis * Vector3(swing_force, 0.0, 0.0)
+			last_swing_right = Time.get_ticks_msec()
+		elif Input.is_action_just_pressed("forward") and time_passed(last_swing_forward, swing_rate):
+			velocity += body.basis * Vector3(0.0, 0.0, -swing_force)
+			last_swing_forward = Time.get_ticks_msec()
+		elif Input.is_action_just_pressed("backward") and time_passed(last_swing_backward, swing_rate):
+			velocity += body.basis * Vector3(0.0, 0.0, swing_force)
+			last_swing_backward = Time.get_ticks_msec()
 	
 	var rope = state_machine.rope_object
 	if rope and rope.is_grappling:
@@ -93,4 +111,8 @@ func PhysicsUpdate(body, delta):
 
 func Exit(body):
 	body.last_direction = direction
+	
+
+func time_passed(last_time : int, minimum_time : int):
+	return Time.get_ticks_msec() - last_time >= minimum_time * 1000
 	
