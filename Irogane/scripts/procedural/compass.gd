2f5c3d47abe804ca2fc367c1b31f7e063055e4a7
StamLord <42681607+StamLord@users.noreply.github.com>
2024-06-14 10:47:44 +0300

    compass fixed rotation

--- Irogane/scripts/procedural/compass.gd
+++ Irogane/scripts/procedural/compass.gd
@@ -4,5 +4,5 @@ var rotation_speed = 4.0
 
 func _process(delta):
 	var parent_rotation = owner.global_rotation
-	rotation.y = lerp_angle(rotation.y, parent_rotation.y, rotation_speed * delta)
+	rotation.y = lerp_angle(rotation.y, -parent_rotation.y, rotation_speed * delta)
 	
