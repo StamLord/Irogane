e24436cd86720062d815c01cc0e9fb92e08690a9
StamLord <42681607+StamLord@users.noreply.github.com>
2024-07-20 17:22:41 +0300

    Update PlayerState.gd

--- Irogane/scripts/controls/player_state_machine/PlayerState.gd
+++ Irogane/scripts/controls/player_state_machine/PlayerState.gd
@@ -2,6 +2,7 @@ extends Node
 class_name PlayerState
 
 var state_machine : PlayerStateMachine
+var recent_collisions = {}
 
 signal Transitioned(state, new_state_name)
 
@@ -26,14 +27,20 @@ func PerformCollisions(body, speed, push_force, delta):
 		var collision = body.get_slide_collision(i)
 		var collider = collision.get_collider()
 		if collider is RigidBody3D:
+			# Ignore recente collisions
+			if recent_collisions.has(collider):
+				var time = Time.get_ticks_msec()
+				if time - recent_collisions[collider] <= 250:
+					continue
+			
 			# Ignore collisions for objects we stand on 
 			# We test for less than .01 difference in height
-			if body.position.y > collision.get_position().y:
+			if body.position.y - collision.get_position().y < 0.01:
 				print("Skip Collision")
 				continue
 			
 			collider.apply_central_impulse(-collision.get_normal() * speed * push_force * delta)
-			collider.apply_impulse(-collision.get_normal() * 0.01, collision.get_position())
+			recent_collisions[collider] = Time.get_ticks_msec()
 		elif collider is CharacterBody3D:
 			collider.velocity += -collision.get_normal() * speed * push_force * delta
 	
