6633a253cc8752be7d817791e125606801b780f1
StamLord <42681607+StamLord@users.noreply.github.com>
2024-08-25 16:36:01 +0300

    Update telescope.gd

--- Irogane/scripts/weapons/telescope.gd
+++ Irogane/scripts/weapons/telescope.gd
@@ -1,22 +1,28 @@
 extends Node3D
 
 @onready var telecam = $telecam
+@onready var mouse_look = $"../../.."
 @export var min_fov = 70.0
 @export var max_fov = 1.0
+@export var min_sensitivity = 0.04
 @export var steps = 10
 
 var current_step = 0
+var base_mouse_sensitivity = 0.4
 
 signal telescope_on(state)
 
 func _ready():
 	telecam.fov = min_fov
 	
+	if mouse_look:
+		base_mouse_sensitivity = mouse_look.mouse_sensitivity
+	
+	visibility_changed.connect(visibility_change)
+	
 
 func _process(delta):
 	if not visible:
-		if telecam.current:
-			set_telecam_active(false)
 		return
 	
 	set_telecam_active(Input.is_action_pressed("attack_secondary"))
@@ -34,9 +40,20 @@ func zoom_to(step):
 	var t = step / float(steps)
 	telecam.fov = lerp(min_fov, max_fov, t)
 	
+	if mouse_look:
+		mouse_look.mouse_sensitivity = lerp(base_mouse_sensitivity, min_sensitivity, t)
+	
 
 func set_telecam_active(state):
 	telecam.current = state
 	CameraEntity.main_camera.current = !state
 	telescope_on.emit(state)
+	
+	if not state: # Reset when closed
+		zoom_to(0)
+	
+
+func visibility_change():
+	if not visible:
+		set_telecam_active(false)
 	
