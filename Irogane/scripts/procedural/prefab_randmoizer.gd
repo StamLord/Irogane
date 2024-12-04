766b3f2a7249b3ed3df0a269dd87b44694f7709d
StamLord <42681607+StamLord@users.noreply.github.com>
2024-12-04 22:20:04 +0200

    Hearth district + prefab_randomizer refactor

--- Irogane/scripts/procedural/prefab_randmoizer.gd
+++ Irogane/scripts/procedural/prefab_randmoizer.gd
@@ -1,9 +1,11 @@
 @tool
 extends Node3D
 
+@onready var variations_parent = $variations
+
 @export var override = -1:
 	set(value):
-		value = clampi(value, -1, get_child_count() - 1)
+		value = clampi(value, -1, get_variations_count() - 1)
 		override = value
 		if override >= 0:
 			update_prefab(override)
@@ -31,13 +33,27 @@ func _process(delta: float) -> void:
 
 func randomize_prefab():
 	seed(global_position.x + global_position.y + global_position.z)
-	var variance = randi_range(0, get_child_count() - 1)
+	var variance = randi_range(0, get_variations_count() - 1)
 	update_prefab(variance)
 	
 
 func update_prefab(id):
 	var i = 0
-	for child in get_children():
-		child.visible = i == id
+	for variation in get_variations():
+		variation.visible = i == id
 		i += 1
+	
+
+func get_variations_count():
+	if not variations_parent:
+		return 0
+	
+	return variations_parent.get_child_count()
+	
+
+func get_variations():
+	if not variations_parent:
+		return []
+	
+	return variations_parent.get_children()
 	
