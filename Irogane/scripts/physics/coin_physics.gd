ae71d527865e9130ad6678bf14a7a5ae344f2d08
StamLord <42681607+StamLord@users.noreply.github.com>
2024-12-27 23:12:25 +0200

    Improved coin

---
+++ Irogane/scripts/physics/coin_physics.gd
@@ -0,0 +1,67 @@
+extends Node3D
+
+@onready var visual = $visual
+
+const GRAVITY = -9.8
+const CLIP_OFFSET = 0.01
+
+var velocity = Vector3.ZERO
+var max_bounces = 3
+var bounces = 0
+var bounce_damp = 0.5
+var killed_momentum = false
+
+func start_force(force: Vector3):
+	velocity = force
+	bounces = 0
+	killed_momentum = false
+	
+
+func _process(delta):
+	var old_position = global_position
+	var new_position = global_position + velocity * delta
+	velocity.y += GRAVITY * delta
+	
+	var space_state = get_world_3d().direct_space_state
+	var query = PhysicsRayQueryParameters3D.create(global_position, new_position)
+	var collision = space_state.intersect_ray(query)
+	
+	if collision:
+		handle_collision(collision)
+	else:
+		global_position = new_position
+	
+	if killed_momentum:
+		rotate_to_rest(delta)
+	else:
+		spin(old_position, delta)
+	
+
+func handle_collision(collision):
+	# Move to collision point to avoid clipping
+	global_position = collision.position + collision.normal * CLIP_OFFSET
+	
+	# New velocity is bounced off of collision's normal
+	if bounces < max_bounces:
+		velocity = calculate_bounce_velocity(velocity, collision.normal)
+		bounces += 1
+	
+	# If no more bounces, leave only y component for falling
+	# Makes the coin fall if the last collision was against a wall
+	elif not killed_momentum:
+		velocity = Vector3(0, velocity.y, 0)
+		killed_momentum = true
+	
+
+func calculate_bounce_velocity(velocity: Vector3, normal: Vector3):
+	return velocity.bounce(normal) * bounce_damp
+	
+
+func spin(old_position, delta):
+	var actual_speed = (global_position - old_position).length()
+	visual.rotation_degrees.x += actual_speed * 50000 * delta
+	
+
+func rotate_to_rest(delta):
+	visual.global_rotation_degrees.x = 0
+	
--- Irogane/scripts/weapons/coin.gd
+++ Irogane/scripts/weapons/coin.gd
@@ -2,7 +2,7 @@ extends Node3D
 
 @onready var main_camera = $"../.."
 
-var coin_prefab = preload("res://prefabs/coin_prefab.tscn")
+var coin_prefab = preload("res://prefabs/coin_2_prefab.tscn")
 
 var last_owner_postion = Vector3.ZERO
 var owner_velocity = Vector3.ZERO
@@ -12,7 +12,7 @@ func _process(delta):
 		return
 	
 	if Input.is_action_just_pressed("attack_primary"):
-		throw_coin()
+		throw_coin_2()
 	
 
 func _physics_process(delta):
@@ -30,4 +30,15 @@ func throw_coin():
 	coin.linear_velocity = owner_velocity
 	coin.apply_impulse(-main_camera.global_basis.z * 10.0)
 	coin.angular_velocity = main_camera.global_basis * Vector3.RIGHT * 20
+	
+
+func throw_coin_2():
+	var coin = coin_prefab.instantiate()
+	get_tree().root.add_child(coin)
+	coin.name = "CoinPrefab"	# If we don't set the name, 
+								# after the first one the rest will be named @Rigidbody#
+	coin.global_position = global_position - main_camera.global_basis.z * 1.2
+	coin.look_at(main_camera.global_position)
+	
+	coin.start_force(owner_velocity + -main_camera.global_basis.z * 10.0)
 	
