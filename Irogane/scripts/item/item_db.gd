10eaf76ff8c1e728eadbf75a1c1d16ffd70bc2c7
StamLord <42681607+StamLord@users.noreply.github.com>
2025-02-09 01:47:40 +0200

    lockpick pickup added, prison cell key moved

--- Irogane/scripts/item/item_db.gd
+++ Irogane/scripts/item/item_db.gd
@@ -71,6 +71,12 @@ const ITEMS = {
 		"type" : SimpleWeaponManager.tool_type.CANDLE,
 		"pickup" : preload(PICKUP_PATH + "pickup.tscn")
 	},
+	"lockpick_tool" : {
+		"icon" : ICON_PATH + "katana.png",
+		"slot" : "TOOL",
+		"type" : SimpleWeaponManager.tool_type.LOCKPICK,
+		"pickup" : preload(PICKUP_PATH + "pickup.tscn")
+	},
 }
 
 static func get_item(id):
