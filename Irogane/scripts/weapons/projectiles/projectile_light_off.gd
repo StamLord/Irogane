77813aee8f1408b8506ebc52855771813e9d60ff
StamLord <42681607+StamLord@users.noreply.github.com>
2025-08-30 14:28:13 +0300

    Update projectile_light_off.gd

--- Irogane/scripts/weapons/projectiles/projectile_light_off.gd
+++ Irogane/scripts/weapons/projectiles/projectile_light_off.gd
@@ -9,15 +9,12 @@ func _ready():
 
 func impact(projectile):
 	force_shapecast_update()
-	print(get_collision_count())
 	for i in range(get_collision_count()):
 		var collider = get_collider(i)
-		print(collider)
 		if collider is Ignitable:
 			collider.extinguish()
 		
 		if collider is LightSwitch:
-			print(collider.state)
 			if collider.state:
 				collider.use(null)
 	
